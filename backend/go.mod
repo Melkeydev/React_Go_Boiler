a43d5760e1d21005ee3e90cfb4569dc2e164db48
Amokstakov <amoksyak@uoguelph.ca>
2021-11-05 17:33:58 -0700

    Finished rate limiting

--- backend/go.mod
+++ backend/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/lib/pq v1.10.3 // indirect
 	github.com/pascaldekloe/jwt v1.10.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 )
--- backend/types/structures.go
+++ backend/types/structures.go
@@ -1,12 +1,17 @@
-package types 
+package types
 
 type Config struct {
-  Port int
-  Env string
-  Db struct {
-    Dsn string
-  }
-  Jwt struct {
-    Secret string
-  }
+	Port int
+	Env  string
+	Db   struct {
+		Dsn string
+	}
+	Jwt struct {
+		Secret string
+	}
+	Limiter struct {
+		Rps     float64
+		Burst   int
+		Enabled bool
+	}
 }
